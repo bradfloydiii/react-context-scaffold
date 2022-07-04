f87c25c8f436c9bde28037e7ac2cba9230035c01
Bradley Floyd III <bfloydiii@protonmail.com>
2022-07-03 20:58:27 -0400

    finished thoughts

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -2,15 +2,14 @@ import React, { useContext, useEffect } from "react";
 import { VaultContext } from "../../context/Vault";
 import * as authService from "../../context/actions/auth";
 
-import { Alert, Button } from "@mui/material";
+import { Alert } from "@mui/material";
 import { v4 as uuidv4 } from "uuid";
 
 const Auth = () => {
   const { state, dispatch } = useContext(VaultContext);
 
   useEffect(() => {
-    // REST call to /api/users/json
-    // Actions.getUsers(dispatch);
+    authService.getUsers(dispatch);
   }, []);
 
   useEffect(() => {
@@ -19,24 +18,15 @@ const Auth = () => {
 
   let loading = false;
 
-  const getUsers = () => {
-    loading = true;
-    authService.getUsers(dispatch);
-  };
-
   return (
     <>
       {state.error && state.error.users && (
         <Alert severity="error">
-          {state.error.name}: {state.error.message}
+          {state.error.users.name}: {state.error.users.message}
         </Alert>
       )}
 
-      <Button onClick={() => getUsers()}>Get User Data</Button>
-
-      {loading && <Alert severity="info">
-          loading user data...
-        </Alert>}
+      {loading && <Alert severity="info">loading user data...</Alert>}
       {state.users && <h3>user names</h3>}
       {state.users &&
         state.users.map((user) => <div key={uuidv4()}>{user.name}</div>)}
--- src/components/Hiking/index.js
+++ src/components/Hiking/index.js
@@ -3,27 +3,22 @@ import React, { useContext, useEffect } from "react";
 import { VaultContext } from "../../context/Vault";
 import * as hikingService from "../../context/actions/hiking";
 
-import { Alert, Button } from "@mui/material";
+import { Alert } from "@mui/material";
 import { v4 as uuidv4 } from "uuid";
 
 const Hiking = () => {
-  const { state, dispatch } = useContext(VaultContext); // subscribes to global state
-  
-  // subscribes the component to changes in state.hikers
+  const { state, dispatch } = useContext(VaultContext);
+
+  useEffect(() => {
+    hikingService.getHikers(dispatch);
+  }, []);
+
   useEffect(() => {
     loading = false;
   }, [state.hikers]);
 
-
-  // local vars and methods
   let loading = false;
 
-  const getHikers = () => {
-    loading = true;
-    hikingService.getHikers(dispatch);
-  };
-
-  // render
   return (
     <>
       {state.error && state.error.hikers && (
@@ -31,8 +26,6 @@ const Hiking = () => {
           {state.error.hikers.name}: {state.error.hikers.message}
         </Alert>
       )}
-
-      <Button onClick={() => getHikers()}>Get Hikers</Button>
 
       {loading && <Alert severity="info">loading user data...</Alert>}
       {state.hikers && <h3>hiker names</h3>}
